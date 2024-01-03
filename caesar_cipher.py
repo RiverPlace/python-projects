93b2ce25cb0f710cffd0c36f75a44f2f0427b759
River Place <riverplace@Rivers-MacBook-Air.local>
2024-01-03 16:04:44 +0800

    Consolidated code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,25 +4,17 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
 
-def encrypt(text, shift):
-    encrypted_text = ''
+def encode_decode(text, shift, direction):
+    new_text = ''
     
     for letter in text:
-        shifted_index = alphabet.index(letter) + shift
-        encrypted_text += alphabet[shifted_index]
-    
-    print(f'Encrypted text: {encrypted_text}')
+        index = ''
+        if direction == 'encode':
+            index = alphabet.index(letter) + shift
+        else:
+            index = alphabet.index(letter) - shift
+        new_text += alphabet[index]
 
-def decrypt(text, shift):
-    decrypted_text = ''
+    print(f'{direction.title()}ed text is: {new_text}')
     
-    for letter in text:
-        restored_index = alphabet.index(letter) - shift
-        decrypted_text += alphabet[restored_index]
-        
-    print(f'Decrypted text: {decrypted_text}')
-    
-if direction == 'encrypt':
-    encrypt(text, shift)
-elif direction == 'decrypt':
-    decrypt(text, shift)
+encode_decode(text, shift, direction)
