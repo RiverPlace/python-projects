a344d737675b5f336a72ecbde4775b96bbd13a2d
River Place <riverplace@Rivers-MacBook-Air.local>
2024-01-03 11:36:22 +0800

    Finished hangman game

--- hangman.py
+++ hangman.py
@@ -1,9 +1,66 @@
 import random
 
 # Step 1
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ['ardvark', 'baboon', 'camel']
-
 chosen_word = random.choice(word_list)
+lives = 6
+end_of_game = False
 
 display = []
 for _ in range(len(chosen_word)):
@@ -14,13 +71,19 @@ def check_guess():
         letter = chosen_word[position]
         if guess == letter:
             display[position] = letter
-
-end_of_game = False
+            
 while not end_of_game:
     guess = input('Please pick a random letter: ').lower()
     
     check_guess()
     print(display)
+    
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print('You lose.')
     
     if '_' not in display:
         end_of_game = True
