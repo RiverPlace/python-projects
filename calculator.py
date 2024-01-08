ce7fcd7f1c0af1cd068529771427eb05568689ec
River Place <riverplace@Rivers-MacBook-Air.local>
2024-01-08 19:58:02 +0800

    Added recursion to restart calc

--- calculator.py
+++ calculator.py
@@ -12,26 +12,24 @@ equation = {
     'num2': 0,
     'result': 0,
 }
-should_continue = True
 
-equation['num1'] = float(input("What's your first number? "))
+def calculator():  
+    equation['num1'] = float(input("What's your first number? "))    
+    
+    should_continue = True
+    while should_continue:
+        equation['operator'] = input("Pick an operation (+ - * /): ")
+        equation['num2'] = float(input("What's your next number? "))
+        equation['result'] = ops[equation['operator']](equation['num1'], equation['num2'])
+        
+        print(f'{equation["num1"]} {equation["operator"]} {equation["num2"]} = {equation['result']}')
+        
+        continue_calc = input(f'Do you want to perform another calculation on {equation['result']}? y = yes, n = no: ')
+        
+        if continue_calc == 'y':
+            equation['num1'] = equation['result']
+        else:
+            should_continue = False
+            calculator()
 
-def get_equation():
-    equation['operator'] = input("Pick an operation (+ - * /): ")
-    equation['num2'] = float(input("What's your next number? "))
-    
-def display_result():
-    equation['result'] = ops[equation['operator']](equation['num1'], equation['num2'])
-    print(f'{equation["num1"]} {equation["operator"]} {equation["num2"]} = {equation['result']}')
-
-while should_continue:
-    get_equation()
-    display_result()
-    
-    continue_calc = input(f'Do you want to perform another calculation on {equation['result']}? y = yes, n = no: ')
-    
-    if continue_calc == 'y':
-        equation['num1'] = equation['result']
-    else:
-        should_continue = False
-    
+calculator()
