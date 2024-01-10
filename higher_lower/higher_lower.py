b8038db553d53503c4c556879f17428f6b5b94e0
River Place <riverplace@Rivers-MacBook-Air.local>
2024-01-10 14:50:04 +0800

    Wrote requirements for higher_lower game

---
+++ higher_lower/higher_lower.py
@@ -0,0 +1,14 @@
+""" REQUIREMENTS
+1. Print logo
+2. Write 1st comparison description
+3. Print 'vs' logo
+4. Write 2nd comparison description
+5. Get user input on who they think has the highest follower count
+6. If correct: 
+    - their choice is sent to 1st comparison spot
+    - new 2nd comparison is displayed 
+    - score is increased by 1
+7. If incorrect, they lose the game
+    - score is displayed
+"""
+
